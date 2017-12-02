ccdd8e4b247cc8a7cb1afba05c71252bcb5a9bf1
野村敏昭 <toshiaki0315@gmail.com>
2017-12-02 23:41:44 +0900

    第八章完了

--- Python/Money.py
+++ Python/Money.py
@@ -4,3 +4,16 @@ class Money:
     def __eq__(self, money):
         return self.amount == money.amount and self.__class__ == money.__class__
 
+    def times(self, multiplier):
+        raise NotImplementedError
+    
+    @staticmethod
+    def dollar(amount):
+        return Dollar(amount)
+
+    @staticmethod
+    def franc(amount):
+        return Franc(amount)
+
+from Dollar import Dollar
+from Franc import Franc
--- Python/MoneyTest.py
+++ Python/MoneyTest.py
@@ -14,29 +14,29 @@
 # □ timesの一般化
 # ■ FrancとDollarを比較する
 # □ 通貨の概念
+# □ testFrancMultiplicationを削除する？
 
 import unittest
-from Dollar import Dollar
-from Franc import Franc
+from Money import Money
 
 class MoneyTest(unittest.TestCase):
   
     def testMultiplication(self):
-        five = Dollar(5)
-        self.assertEqual(Dollar(10), five.times(2))
-        self.assertEqual(Dollar(15), five.times(3))
+        five = Money.dollar(5)
+        self.assertEqual(Money.dollar(10), five.times(2))
+        self.assertEqual(Money.dollar(15), five.times(3))
   
     def testEquality(self):
-        self.assertTrue(Dollar(5).__eq__(Dollar(5)))
-        self.assertFalse(Dollar(5).__eq__(Dollar(6)))
-        self.assertTrue(Franc(5).__eq__(Franc(5)))
-        self.assertFalse(Franc(5).__eq__(Franc(6)))
-        self.assertFalse(Franc(5).__eq__(Dollar(5)))
+        self.assertTrue(Money.dollar(5).__eq__(Money.dollar(5)))
+        self.assertFalse(Money.dollar(5).__eq__(Money.dollar(6)))
+        self.assertTrue(Money.franc(5).__eq__(Money.franc(5)))
+        self.assertFalse(Money.franc(5).__eq__(Money.franc(6)))
+        self.assertFalse(Money.franc(5).__eq__(Money.dollar(5)))
 
     def testFrancMultiplication(self):
-        five = Franc(5)
-        self.assertEqual(Franc(10), five.times(2))
-        self.assertEqual(Franc(15), five.times(3))
+        five = Money.franc(5)
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
 
 
 if __name__ == '__main__':
