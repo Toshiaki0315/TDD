f9460ab79e8838ed93b7ee850a93f46f74acd060
野村敏昭 <toshiaki0315@gmail.com>
2017-12-02 22:28:01 +0900

    Dollarの__eq__をMoneyに移動

--- Python/Dollar.py
+++ Python/Dollar.py
@@ -1,11 +1,8 @@
-class Dollar:
-  __amount = 0
+from Money import Money
+
+class Dollar(Money):
   def __init__(self, amount):
-    self.__amount = amount
+    self.amount = amount
   
   def times(self, multiplier):
-    return Dollar(self.__amount * multiplier)
-
-  def __eq__(self, dollar):
-    return self.__amount == dollar.__amount
-
+    return Dollar(self.amount * multiplier)
