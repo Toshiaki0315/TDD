830c176439ada5b0023e2ab0e451ade9e7eef658
野村敏昭 <toshiaki0315@gmail.com>
2017-12-02 22:41:29 +0900

    第七章完了

--- Python/Money.py
+++ Python/Money.py
@@ -2,4 +2,5 @@ class Money:
     amount = 0
 
     def __eq__(self, money):
-        return self.amount == money.amount
+        return self.amount == money.amount and self.__class__ == money.__class__
+
--- Python/MoneyTest.py
+++ Python/MoneyTest.py
@@ -12,7 +12,8 @@
 # □ DollarとFrancの重複
 # ■ equalsの一般化
 # □ timesの一般化
-# □ FrancとDollarを比較する
+# ■ FrancとDollarを比較する
+# □ 通貨の概念
 
 import unittest
 from Dollar import Dollar
